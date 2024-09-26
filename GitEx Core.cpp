fcfee854b79bb0b7238a5ba53bb8bf52bc1f9c77
CalamityMak <Connorgmckenzie1@gmail.com>
2024-09-26 13:28:01 -0500

    modified b2 line 11

--- GitEx Core.cpp
+++ GitEx Core.cpp
@@ -7,10 +7,6 @@ int main()
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Hello World!" << std::endl;
-<<<<<<< HEAD
 	std::cout << "Hello World! I love this Line!" << std::endl;
-=======
-	std::cout << "Hello World!" << std::endl;
->>>>>>> b1
-	std::cout << "Hello World!" << std::endl;
+    std::cout << "Hello World! This line is awesome, I must have it!" << std::endl;
 }

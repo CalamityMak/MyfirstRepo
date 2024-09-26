37c38953c77a858bbaf7a29f4bfae31bbf37838e
CalamityMak <Connorgmckenzie1@gmail.com>
2024-09-26 13:10:18 -0500

    modfied files on b1

--- GitEx Core.cpp
+++ GitEx Core.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
-  
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
 }

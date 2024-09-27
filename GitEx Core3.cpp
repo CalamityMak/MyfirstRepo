500a14e1639c3e67848148b446c6153ec3633353
CalamityMak <Connorgmckenzie1@gmail.com>
2024-09-26 21:09:46 -0500

    modified GitEx Core.cpp

--- GitEx Core3.cpp
+++ GitEx Core3.cpp
@@ -9,4 +9,5 @@ int main()
 	std::cout << "Hello World!" << std::endl;
 	std::cout << "Hello World! I love this Line!" << std::endl;
     std::cout << "Hello World! This line is awesome, I must have it!" << std::endl;
+	std::cout << "Must have this line!" << std::endl;
 }
